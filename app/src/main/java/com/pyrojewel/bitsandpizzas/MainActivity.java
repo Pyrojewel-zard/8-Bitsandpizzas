597c1d77e596d153929e46f7f95b19d3db48c3ad
Pyrojewel <2895687337@qq.com>
2022-01-25 16:00:13 +0800

    12章最终的实现，值得一提的是，在之前的menu_main.xml中，由于actionProviderClass设置错误，导致Order界面一直出不来，注意android和androidx版本之间不兼容！！！

--- app/src/main/java/com/pyrojewel/bitsandpizzas/MainActivity.java
+++ app/src/main/java/com/pyrojewel/bitsandpizzas/MainActivity.java
@@ -2,10 +2,13 @@ package com.pyrojewel.bitsandpizzas;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.ShareActionProvider;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
@@ -20,21 +23,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar=(Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        //关联
-        SectionsPagerAdapter pagerAdapter=
+
+        //Attach the SectionsPagerAdapter to the ViewPager
+        SectionsPagerAdapter pagerAdapter =
                 new SectionsPagerAdapter(getSupportFragmentManager());
-        ViewPager pager=(ViewPager) findViewById(R.id.pager);
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
         pager.setAdapter(pagerAdapter);
-        TabLayout tabLayout=(TabLayout) findViewById(R.id.tabs);
+
+        //Attach the ViewPager to the TabLayout
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(pager);
     }
-    private void setShareActionInten(String text){
-        Intent intent=new Intent(Intent.ACTION_SEND);
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        shareActionProvider =
+                (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        setShareActionIntent("Want to join me for pizza?");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void setShareActionIntent(String text) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
-        intent.putExtra(Intent.EXTRA_TEXT,text);
+        intent.putExtra(Intent.EXTRA_TEXT, text);
         shareActionProvider.setShareIntent(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_create_order:
+                Intent intent = new Intent(this, OrderActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private class SectionsPagerAdapter extends FragmentPagerAdapter {
@@ -51,13 +81,13 @@ public class MainActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             switch (position) {
                 case 0:
-                    return new com.pyrojewel.bitsandpizzas.TopFragment();
+                    return new TopFragment();
                 case 1:
                     return new PizzaFragment();
                 case 2:
                     return new PasteFragment();
                 case 3:
-                    return new com.pyrojewel.bitsandpizzas.StoresFragment();
+                    return new StoresFragment();
             }
             return null;
         }
@@ -78,3 +108,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+
--- app/src/main/java/com/pyrojewel/bitsandpizzas/OrderActivity.java
+++ app/src/main/java/com/pyrojewel/bitsandpizzas/OrderActivity.java
@@ -1,10 +1,15 @@
 package com.pyrojewel.bitsandpizzas;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;//导入toolbar
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -13,8 +18,24 @@ public class OrderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);//必然的操作，因为需要使用支持库中的工具条，将工具条替换了应用条
+        setSupportActionBar(toolbar);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClickDone(View view) {
+        CharSequence text = "Your order has been updated";
+        int duration = Snackbar.LENGTH_SHORT;
+        Snackbar snackbar = Snackbar.make(findViewById(R.id.coordinator), text, duration);
+        snackbar.setAction("Undo",
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast toast = Toast.makeText(OrderActivity.this, "Undone!",
+                                Toast.LENGTH_SHORT);
+                        toast.show();
+                    }
+                });
+        snackbar.show();
     }
 }
